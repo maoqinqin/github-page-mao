16f4ebef2fd3bdcb53b6404dcce5b0faeaa042dc
qinqin.mao <qinqin.mao@mhp.com>
2020-04-23 01:58:13 +0800

    feat:三次提交

--- example/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import ReactDemo from '../../src/component';
-
-const App = () => <ReactDemo />;
-
-render(<App />, document.getElementById('root'));
--- src/component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ReactDemo = () => (
-    <h1>这是我的第一个 11111111 react npm 组件</h1>
-);
-
-export default ReactDemo;
